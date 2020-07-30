55f28c5bbe109d87ad553211d37741f238f243e9
prem0406 <premk191@gmail.com>
2020-07-30 14:18:32 +0530

    product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((res: Response)=> res.json());
+    return this._http.get(this._albumUrl).map((response: Response)=> response.json());
   }
 
 }
